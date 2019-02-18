c6c63770c9ceb98bb5e25d99704f839dda361f6f
SungchulLee <sungchul@yonsei.ac.kr>
2019-02-18 17:54:00 -0600

    add dqn_ops.py

---
+++ dqn_ops.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+
+
+def variable_copy(from_scope, to_scope):
+    """
+    It assigns variables of "from_scope" to variables of "to_scope".
+    :param from_scope: string
+    :param to_scope: string
+    :return:
+    """
+
+    from_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, from_scope)
+    to_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, to_scope)
+
+    copy_ops = [to_vars[i].assign(from_vars[i]) for i in range(len(from_vars))]
+
+    return copy_ops
+
+
+def one_hot_encode(states, num_states):
+    """
+    :param states: list or 1D numpy array, ie: [1,2,5,7]
+    :param num_states: integer; number of states
+    :return: numpy 2D array using one-hot encoding.
+    """
+    return np.eye(num_states)[states].astype(np.float32)
+
+
+if __name__ == '__main__':
+    states = [3]
+    num_states = 11
+    encoding = one_hot_encode(states, num_states)
+    print(encoding)
+    print()
+
+    states = [3, 5]
+    num_states = 11
+    encoding = one_hot_encode(states, num_states)
+    print(encoding)
+    print()
+
+    states = np.array([3, 5])
+    num_states = 11
+    encoding = one_hot_encode(states, num_states)
+    print(encoding)
