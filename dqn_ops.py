51201bb56554146b1060df9a28208768e471ebdf
SungchulLee <sungchul@yonsei.ac.kr>
2019-03-15 22:13:25 +0900

    some comments are added

--- dqn_ops.py
+++ dqn_ops.py
@@ -5,13 +5,16 @@ import numpy as np
 def variable_copy(from_scope, to_scope):
     """
     It assigns variables of "from_scope" to variables of "to_scope".
-    :param from_scope: string
-    :param to_scope: string
+    :param from_scope: string, for example, Qnet.name
+    :param to_scope: string, for example, Qnet_target.name
     :return:
     """
 
     from_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, from_scope)
+    # [<tf.Variable 'qnet/weight1:0' shape=(11, 4) dtype=float32_ref>]
+
     to_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, to_scope)
+    # [<tf.Variable 'target_qnet/weight1:0' shape=(11, 4) dtype=float32_ref>]
 
     copy_ops = [to_vars[i].assign(from_vars[i]) for i in range(len(from_vars))]
 
